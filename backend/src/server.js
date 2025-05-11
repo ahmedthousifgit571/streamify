d0d790a2687f5128262ed1b2d56edd97eb667920
ahmed thousif <ahmedthousif571@gmail.com>
2025-05-11 16:21:43 +0530

    signup page ui

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,7 @@ import userRoutes from './routes/user.route.js'
 import chatRoutes from './routes/chat.route.js'
 import { connectDB } from './lib/db.js'
 import cookieParser from 'cookie-parser'
-
+import cors from 'cors'
 
 dotenv.config()
 
@@ -14,6 +14,10 @@ const PORT = process.env.PORT
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 
 app.use("/api/auth",authRoutes)
 app.use("/api/users",userRoutes)
