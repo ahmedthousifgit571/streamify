851c1f7c0477720a591f1c8171e0276a7fa1e985
ahmed thousif <ahmedthousif571@gmail.com>
2025-08-05 18:49:06 +0530

    cors link

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,7 +24,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:["http://localhost:5173", "https://streamify-jee4.vercel.app"],
+    origin:["http://localhost:5173", "https://streamify-p7e2.vercel.app"],
     credentials:true
 }))
 
